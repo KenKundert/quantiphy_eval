d0b34e712bb420f82bf6a758f1cfa0b23716e67f
Ken Kundert <ken@theKunderts.net>
2023-01-02 16:54:43 -0800

    update copyright

--- src/quantiphy_eval.py
+++ src/quantiphy_eval.py
@@ -9,7 +9,7 @@ quantities.
 """
 
 # MIT License {{{1
-# Copyright (C) 2020-2022 Kenneth S. Kundert
+# Copyright (C) 2020-2023 Kenneth S. Kundert
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
